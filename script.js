51bc3560d93750e586c1245ffec02deb7db1fc5a
Mirza <35157665+yes-2875@users.noreply.github.com>
2025-10-02 13:31:28 -0400

    Update to finish

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 // If statements
+console.log("If statements:");
 let number = 3;
 
 if (number > 0) {
@@ -12,7 +13,9 @@ else {
 }
 
 // Switch statements
-let day = 4;
+console.log("\nSwitch statements (random number):");
+let day = Math.floor(Math.random() *10 - 1);
+
 switch(day) {
     case 1:
         console.log("Monday");
@@ -41,16 +44,20 @@ switch(day) {
 }
 
 // Loops
+console.log("\nFor loop:");
 for (let i = 1; i <= 5; i++) {
     console.log(i);
 }
 
+console.log("\nWhile loop:");
 let i = 1;
 while (i <= 5) {
     console.log(i);
     i++;
 }
 
+
+console.log("\nDo-while loop:");
 let j = 1;
 do {
     console.log(j);
@@ -58,7 +65,37 @@ do {
 } while (j <= 5);
 
 // Control Flow with break and continue
+console.log("\nControl flow with break and continue:");
+console.log("Break example:");
+for (let k = 1; k <= 10; k++) {
+    if (k === 3) {
+        break; // Exit the loop at k = 3
+    }
+    console.log(k);
+}
 
 
+console.log("Continue example:");
+for (let i = 1; i <= 10; i++) {
+    if (i === 3) {
+        continue; // Skip i = 3
+    }
+    console.log(i);
+}
 
 // Scope and Context
+console.log("\nScope and Context:");
+let globalVariable = "This is a global variable.";
+
+function scopeExample() {
+    let localVariable = "This is a local variable.";
+
+    console.log(globalVariable);
+    console.log(localVariable);
+    console.log("Both local and global variables are accessible inside the function.\n");
+}
+
+scopeExample();
+
+console.log(globalVariable);
+console.log("\nLocal variable is not accessible outside the function.");
